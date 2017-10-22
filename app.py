d09a320a7735b162e1a8e55cc8fc15f1ffa58fc3
Gavin Lifrieri <gavinlifrieri@Gavins-MacBook-Pro.local>
2017-10-21 22:37:01 -0400

    implements getting worst tweet

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
 from helpers import *
 import xlrd
+import tweepy
+import json
 
 
 app = Flask(__name__)
 workbook = xlrd.open_workbook('realDonaldTrump_tweets.xls')
 worksheet = workbook.sheet_by_index(0)
+
+consumer_key = 	"Bfc0WkrjRCxl0AGp3JI2m7QXk"
+consumer_secret = "GyV4dJpLQJfOnELr04tv5vpYt6gGBLEM3sfF4M3imKF1bEj20w"
+access_key = 	"1932838459-IQwTLSlwIcqqm1NPv7eW8S1bqyl2GoPprAAoZ3M"
+access_secret = 	"ELm1Vhsw2zhhny5N3nDM7RVq2giDxm6BUAAFgGCW0DcKQ"
+
+
 
 
 @app.route('/')
@@ -15,14 +24,21 @@ def root():
 @app.route('/<month>/<day>/<year>')
 def handler(day, month, year):
 
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_key, access_secret)
+	api = tweepy.API(auth)
+
 	dateToCheck = Date(day, month, year)
 
 	tweetIndices = getTweets(dateToCheck, worksheet)
 
 	worstTweetIndex = worstTweet(tweetIndices, worksheet)
 
-	tweet = worksheet.cell(worstTweetIndex,2).value
+	statusID = worksheet.cell(worstTweetIndex,0).value
 
+	status = api.get_status(statusID)
+
+	tweet = ''
 
 	return render_template('index.html', year=dateToCheck.year,month=dateToCheck.month,day=dateToCheck.day, tweet=tweet);
 
